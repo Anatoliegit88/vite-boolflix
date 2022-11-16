8e52e7ec396991c70428a713cae69626d0ed5c21
Anatoliegit88 <anatolie.coroban88@gmail.com>
2022-11-16 17:54:10 +0100

    upgrade

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -6,7 +6,7 @@ export default {
   },
   data() {
     return {
-      availableFlags: ["en", "it"],
+      availableFlags: ["italian-flag", "uk-flag"],
     };
   },
   computed: {
@@ -18,22 +18,99 @@ export default {
         ? this.card.original_title
         : this.card.original_title;
     },
+    getImage() {
+      return this.card.poster_path
+        ? `http://image.tmdb.org/t/p/w342/${this.card.poster_path}`
+        : this.getImgUrl("no-image.jpg");
+    },
+    getNumberStars() {
+      return Math.ceil(this.card.vote_average / 2);
+    },
+
+    methods: {
+      getImgUrl(imgName) {
+        return new URL(`../assets/${imgName}`, import.meta.url).href;
+        // console.log("getImgUrl");
+      },
+    },
   },
 };
 </script>
 
 <template>
-  <div class="conatainer">
-    <h2>{{ card.original_title }}</h2>
-    <div>
-      <img
-        v-if="availableFlags.includes(card.original_language)"
-        :src="`https://image.tmdb.org/t/p/w342/${card.poster_path}`"
-        alt="" />
-      <p>{{ card.vote_average }} <i class="fa-solid fa-star"></i></p>
-      <p>{{ card.original_title }}</p>
+  <!-- CARDS -->
+  <div class="card">
+    <div class="card_inner">
+      <div class="card_front">
+        <img
+          v-if="card.poster_path"
+          :src="`http://image.tmdb.org/t/p/w342/${card.poster_path}`"
+          alt="" />
+        <img v-else src="../assets/images/no-image.jpg" alt="" />
+        <img :src="getImage" alt="" />
+      </div>
+      <div class="card_back">
+        <h2>Title: {{ card.title }}</h2>
+        <h4>Ooriginal Title: {{ card.original_title }}</h4>
+        <div class="flag">
+          <img
+            v-if="availableFlags.includes(card.original_language)"
+            :src="getImgUrl(`${card.original_language}.jpg`)" />
+          <p v-else>Language: {{ card.original_language }}</p>
+        </div>
+        <div>
+          <h5>Vote: {{ card.vote_average }}</h5>
+          <h5>Vote: {{ getNumberStars }}</h5>
+
+          <i
+            v-for="n in 5"
+            :key="n"
+            :class="n <= getNumbersStars ? 'fa-solid' : 'fa-regular'"
+            class="fa-star"></i>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.card {
+  height: 100%;
+  perspective: 1000px;
+  &__inner {
+    height: 100%;
+    position: relative;
+    transform-style: preserve-3d;
+    transition: transform 0.8s;
+  }
+  &:hover &__inner {
+    transform: rotateY(180deg);
+  }
+  &__front,
+  &__back {
+    backface-visibility: hidden;
+  }
+  &__front {
+    height: 100%;
+    img {
+      height: 100%;
+    }
+  }
+  &__back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    overflow-y: auto;
+    background-color: rgba(#000000, 0.8);
+    color: white;
+    padding: 1rem;
+    transform: rotateY(180deg);
+
+    .flag {
+      width: 50px;
+    }
+  }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,10 +15,16 @@ export default {
 </script>
 
 <template>
-  <main>
+  <main class="big-box">
     <section>
       <h3>Movies</h3>
-      <AppCard v-for="card in store.movies" :key="card.id" :card="card" />
+      <div class="box">
+        <AppCard
+          class="flex-box"
+          v-for="card in store.movies"
+          :key="card.id"
+          :card="card" />
+      </div>
     </section>
     <section>
       <h3>Tv-Series</h3>
@@ -26,3 +32,24 @@ export default {
     </section>
   </main>
 </template>
+
+<style lang="scss" scoped>
+section {
+  h3 {
+    font-size: 1.5rem;
+    text-transform: uppercase;
+    margin-bottom: 1em;
+  }
+  .box {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .flex-box {
+    padding: 0.5rem;
+    width: calc(100% / 4 - 10px);
+    margin-bottom: 1rem;
+    margin-left: 5px;
+    margin-right: 5px;
+  }
+}
+</style>
